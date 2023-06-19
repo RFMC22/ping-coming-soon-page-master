337ef0c288b8045a797753ae17a83e7d1db4b307
Renee Magallanes <rene22797@gmail.com>
2023-06-19 17:46:37 -0500

    solventando Links must have discernible text

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -6,13 +6,13 @@ const Brands = ({copyright}) => {
     <div className='mt-[72px] mx-auto w-[204px] h-[71px] flex flex-col justify-between items-center mb-[43px]'>
       <div className='flex justify-evenly items-center w-[120px]'>
       <a href='https://www.facebook.com/' target='_blank' className='w-[32px] h-[32px] border border-blue-extend-50 rounded-full flex justify-center items-center group hover:bg-blue-extend-500'>
-        <FontAwesomeIcon icon={faFacebookF} className='text-blue-extend-500 group-hover:text-white'/>
+        <span><FontAwesomeIcon icon={faFacebookF} className='text-blue-extend-500 group-hover:text-white'/></span>
       </a>
       <a href='https://twitter.com/' target='_blank' className='w-[32px] h-[32px] border border-blue-extend-50 rounded-full flex justify-center items-center group hover:bg-blue-extend-500'>
-        <FontAwesomeIcon icon={faTwitter} className='text-blue-extend-500 group-hover:text-white'/>
+        <span><FontAwesomeIcon icon={faTwitter} className='text-blue-extend-500 group-hover:text-white'/></span>
       </a>
       <a href='https://www.instagram.com/' target='_blank' className='w-[32px] h-[32px] border border-blue-extend-50 rounded-full flex justify-center items-center group hover:bg-blue-extend-500'>
-        <FontAwesomeIcon icon={faInstagram} className='text-blue-extend-500 group-hover:text-white'/>
+        <span><FontAwesomeIcon icon={faInstagram} className='text-blue-extend-500 group-hover:text-white'/></span>
       </a>
       </div>
       <p className='text-center text-gray text-xs'>{copyright}</p>
